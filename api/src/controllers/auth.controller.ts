5e73c4d162f78a03620da6cfb9af58853858fc03
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-02 21:21:47 +0330

    ✨ (api) implement sign-up functionality with user validation and error handling

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,13 +1,49 @@
-import { type Request, type Response } from 'express';
+import type { Request, Response } from 'express';
 
-export function signUp(req: Request, res: Response) {
-  res.send('Sign Up Route');
+import { AppError } from '../lib/errors';
+import { UserService } from '../services/user.services';
+
+export async function signUp(req: Request, res: Response) {
+  try {
+    const { fullname, email, password } = req.body;
+
+    const { user } = await UserService.signup(
+      {
+        fullname,
+        email,
+        password,
+      },
+      res,
+    );
+
+    res.status(201).json({
+      status: 'success',
+      data: { user },
+    });
+  } catch (error) {
+    if (error instanceof AppError) {
+      res.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    } else {
+      console.error('Error in Sign Up Controller:', error);
+      res.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error',
+      });
+    }
+  }
 }
 
-export function logIn(req: Request, res: Response) {
+// ********************************************
+
+export async function logIn(req: Request, res: Response) {
   res.send('Log In Route');
 }
 
-export function logOut(req: Request, res: Response) {
+// ********************************************
+
+export async function logOut(req: Request, res: Response) {
   res.send('Log Out Route');
 }
---
+++ api/src/services/user.services.ts
@@ -0,0 +1,70 @@
+import type { Response } from 'express';
+import type mongoose from 'mongoose';
+
+import { PASSWORD_MIN_LENGTH } from '../lib/constants';
+import { ValidationError } from '../lib/errors';
+import { generateToken } from '../lib/generate-token';
+import { hashPassword } from '../lib/hash-password';
+import { User } from '../models/user.model';
+
+type SignupInput = {
+  email: string;
+  fullname: string;
+  password: string;
+};
+
+type UserResponse = {
+  _id: mongoose.Types.ObjectId;
+  fullname: string;
+  email: string;
+  profilePic: string;
+};
+
+export const UserService = {
+  async signup(
+    data: SignupInput,
+    res: Response,
+  ): Promise<{ user: UserResponse }> {
+    const { email, fullname, password } = data;
+    const normalizedEmail = email.toLowerCase();
+
+    if (password.length < PASSWORD_MIN_LENGTH) {
+      throw new ValidationError(
+        `Password must be at least ${PASSWORD_MIN_LENGTH} characters.`,
+      );
+    }
+
+    const existingUser = await User.findOne({
+      equals: { email: normalizedEmail },
+    });
+
+    if (existingUser) {
+      throw new ValidationError('Email is already in use');
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = await User.create({
+      fullname,
+      email: normalizedEmail,
+      password: hashedPassword,
+    });
+
+    if (!newUser) {
+      throw new ValidationError('Invalid user data');
+    }
+
+    generateToken(newUser._id, res);
+
+    await newUser.save();
+
+    return {
+      user: {
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      },
+    };
+  },
+};
